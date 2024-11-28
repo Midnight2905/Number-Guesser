04e16ddcc46b0eb83f3117e547401e9afc1ce5ff
Midnight2905 <curtislussier12@gmail.com>
2024-11-28 14:06:03 -0500

    Update Number Guesser.py
    Fixed

--- Number Guesser.py
+++ Number Guesser.py
@@ -1,13 +1,18 @@
 #Number Guesser
 import random
-randNum = random.randint(0, 999)
+randNum = random.randint(0, 100)
 while True:
     usrGuess = input("Guess a number: ")
-    if(int(usrGuess) > randNum):
-     print("To high")
-    if(int(usrGuess) < randNum):
-     print("To low")
-    if(int(usrGuess) == randNum):
-      print("You guessed correct!!!")
-      quit()
-    
+    #checks to see if the user entered an int
+    try:
+      usrGuess = int(usrGuess)
+      if(int(usrGuess) ==  randNum):
+       print("You guessed correct!!!")
+       quit()
+      elif(int(usrGuess) < randNum):
+       print("To low")
+      elif(int(usrGuess) > randNum):
+       print("To High")
+      
+    except:
+      print("Please enter an int")
